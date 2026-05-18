580dd228a3d3254544e45dc8234b97249688d69c
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2026-05-18 11:55:42 -0700

    a

--- src/main/java/at/hannibal2/skyhanni/features/garden/sensitivity/MouseSensitivityManager.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/sensitivity/MouseSensitivityManager.kt
@@ -3,7 +3,6 @@ package at.hannibal2.skyhanni.features.garden.sensitivity
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.events.DebugDataCollectEvent
-import at.hannibal2.skyhanni.features.garden.sensitivity.SensitivityReducer.REDUCING_FACTOR_HARD_BOUNDS
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 
 @SkyHanniModule
@@ -12,46 +11,40 @@ object MouseSensitivityManager {
     private val config get() = SkyHanniMod.feature.garden.sensitivityReducer
 
     private var state: SensitivityState = SensitivityState.UNCHANGED
-
-    private var lastIn = Float.NaN
-    private var lastOut = Float.NaN
-
-    fun getSensitivity(original: Float): Float {
-        if (original != lastIn) {
-            lastIn = original
-            lastOut = state.apply(original)
+        set(value) {
+            field = value
+            lastOriginal = Float.NaN
+            lastReduced = Float.NaN
         }
 
-        return lastOut
+    private var lastOriginal = Float.NaN
+    private var lastReduced = Float.NaN
+
+    fun getSensitivity(original: Float): Float {
+        // if sensitivity has not changed, no need to recalculate
+        if (lastOriginal != original) {
+            lastOriginal = original
+            lastReduced = state.transform(original)
+        }
+
+        return lastReduced
     }
 
     @HandleEvent
     fun onDebugDataCollect(event: DebugDataCollectEvent) {
         event.title("Mouse Sensitivity")
-
-        if (SensitivityState.UNCHANGED.isActive()) {
-            event.addIrrelevant("not enabled")
-            return
-        }
-
         event.addData {
             add("current state: $state")
         }
     }
 
-    enum class SensitivityState(private val transform: ((Float) -> Float)) {
+    enum class SensitivityState(val transform: ((Float) -> Float)) {
         UNCHANGED({ it }),
-        REDUCED({ ((it + 1f / 3f) / config.reducingFactor.get().coerceIn(REDUCING_FACTOR_HARD_BOUNDS)) - 1f / 3f }),
-        LOCKED({ -1f / 3f }),
+        REDUCED({ it * config.reducingFactor.get() }),
+        LOCKED({ 0 }),
         ;
 
-        fun apply(original: Float) = transform(original)
         fun isActive() = state == this
-        fun setActive() {
-            if (state == this) return
-            state = this
-            lastIn = Float.NaN
-            lastOut = Float.NaN
-        }
+        fun setActive() = if (state != this) state = this
     }
 }
