6b27b7bd7e18456f5b9bb2999256b1883feecdd5
zumbiepig <121742281+zumbiepig@users.noreply.github.com>
2026-06-25 22:30:37 -0700

    a
    Signed-off-by: zumbiepig <121742281+zumbiepig@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/commands/PreventEarlyCommands.kt
+++ src/main/java/at/hannibal2/skyhanni/features/commands/PreventEarlyCommands.kt
@@ -2,90 +2,118 @@ package at.hannibal2.skyhanni.features.commands
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.data.hypixel.chat.event.SystemMessageEvent
 import at.hannibal2.skyhanni.events.MessageSendToServerEvent
-import at.hannibal2.skyhanni.events.chat.SkyHanniChatEvent
-import at.hannibal2.skyhanni.events.minecraft.WorldChangeEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
-import at.hannibal2.skyhanni.utils.ChatUtils.senderIsSkyhanni
 import at.hannibal2.skyhanni.utils.DelayedRun
 import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.SkyBlockUtils
 import at.hannibal2.skyhanni.utils.StringUtils
 import at.hannibal2.skyhanni.utils.compat.append
-import at.hannibal2.skyhanni.utils.compat.componentBuilder
 import at.hannibal2.skyhanni.utils.compat.withColor
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
+import at.hannibal2.skyhanni.utils.roundedUpSeconds
 import net.minecraft.ChatFormatting
-import kotlin.math.ceil
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
-import kotlin.time.DurationUnit
 
 @SkyHanniModule
 object PreventEarlyCommands {
+
+    private val MESSAGE_ID = ChatUtils.getUniqueMessageId()
+
     private val config get() = SkyHanniMod.feature.misc.commands
 
-    private var commandExecuted: SimpleTimeMark = SimpleTimeMark.farPast()
-    private var worldChanged: SimpleTimeMark = SimpleTimeMark.farPast()
     private var lastCommand: String? = null
+    private var action: (() -> Unit)? = null
+    private var actionId: Int = 0
 
     /**
-     * REGEX-TEST: §cYou may only use this command after 4s on the server!
+     * REGEX-TEST: You may only use this command after 4s on the server!
      */
     private val cooldownPattern by RepoPattern.pattern(
         "commands.cooldown",
-        "§cYou may only use this command after (?<cooldown>\\d+)s on the server!",
+        "You may only use this command after 4s on the server!",
+    )
+
+    /**
+     * REGEX-TEST: Couldn't warp you! Try again later. (PLAYER_TRANSFER_COOLDOWN)
+     * REGEX-FAIL: Unable to use this launch pad at the moment, please try again later. (PLAYER_TRANSFER_COOLDOWN)
+     */
+    private val transferCooldownPattern by RepoPattern.pattern(
+        "commands.transfercooldown",
+        "Couldn't warp you! Try again later\\. \\(PLAYER_TRANSFER_COOLDOWN\\)",
     )
 
     @HandleEvent
-    fun onMessageSendToServer(event: MessageSendToServerEvent) {
-        if (!config.preventEarlyExecution) return
-        if (!SkyBlockUtils.onHypixel) return
-        if (!event.isCommand) return
-        if (event.senderIsSkyhanni()) return
-        val command = event.message.removePrefix("/").lowercase()
-        lastCommand = command
-
-        commandExecuted = SimpleTimeMark.now()
-    }
-
-    @HandleEvent
-    fun onWorldChange(event: WorldChangeEvent) {
-        worldChanged = SimpleTimeMark.now()
+    fun onWorldChange() {
+        // invalidate scheduled commands
         lastCommand = null
+        action = null
+        actionId++
     }
 
-    @HandleEvent
-    fun onChat(event: SkyHanniChatEvent.Allow) {
-        if (!SkyBlockUtils.onHypixel) return
-        if (!config.preventEarlyExecution) return
-        val lastCommand = lastCommand ?: return
-        cooldownPattern.matchMatcher(event.message) {
-            val cooldown = group("cooldown")
-            val runIn: Duration = (cooldown?.toInt()?.seconds ?: 5.seconds) - worldChanged.absoluteDifference(SimpleTimeMark.now())
-            DelayedRun.runDelayed(runIn) {
-                ChatUtils.sendMessageToServer("/$lastCommand")
+    @HandleEvent(onlyOnSkyblock = true)
+    fun onMessageSendToServer(event: MessageSendToServerEvent) {
+        if (!event.isCommand) return
+        lastCommand = event.message
+    }
+
+    @HandleEvent(onlyOnSkyblock = true)
+    fun onSystemMessage(event: SystemMessageEvent.Allow) {
+        val command = lastCommand ?: return
+
+        if (config.preventEarlyExecution)
+            cooldownPattern.matchMatcher(event.cleanMessage) {
+                lastCommand = null
+                event.blockedReason = "prevent_early_command"
+                handleEarlyCommand(command)
+                return
             }
-            event.blockedReason = "prevent_early_command"
-            val seconds = ceil(runIn.toDouble(DurationUnit.SECONDS)).toInt()
-            val formattedTime = "$seconds ${StringUtils.pluralize(seconds, "second")}"
 
-            ChatUtils.chat(
-                componentBuilder {
-                    withColor(ChatFormatting.RED)
-                    append("Cannot execute ")
-                    append("/$lastCommand ") {
-                        withColor(ChatFormatting.YELLOW)
-                    }
-                    append("yet. ")
-                    append("Running it in $formattedTime.") {
-                        withColor(ChatFormatting.GREEN)
+        if (config.transferCooldown)
+            transferCooldownPattern.matchMatcher(event.cleanMessage) {
+                lastCommand = null
+                event.blockedReason = "fix_transfer_cooldown"
+                handleTransferCooldown(command)
+                return
+            }
+    }
 
-                    }
-                }
-            )
+    private fun handleEarlyCommand(command: String) {
+        val untilCooldown = (SkyBlockUtils.lastWorldSwitch + 4.seconds).timeUntil().coerceAtLeast(Duration.ZERO)
+        runAfterDelay(command, untilCooldown)
+    }
+
+    private fun handleTransferCooldown(command: String) {
+        val untilCooldown = (SkyBlockUtils.lastWorldSwitch + 3.seconds).timeUntil()
+
+        if (untilCooldown < 2.seconds) runAfterDelay(command, 2.seconds)
+        else runAfterDelay(command, untilCooldown)
+    }
+
+    private fun runAfterDelay(command: String, delay: Duration) {
+        action = { ChatUtils.sendMessageToServer(command) }
+        val id = ++actionId
+        DelayedRun.runDelayed(delay) {
+            if (actionId != id) return@runDelayed // this is a different command, dont run it yet
+
+            action?.invoke()
+            action = null
+        }
+
+        ChatUtils.chat(messageId = MESSAGE_ID) {
+            withColor(ChatFormatting.RED)
+            append("Cannot execute ")
+            append(command) {
+                withColor(ChatFormatting.YELLOW)
+            }
+            append(" yet. ")
+            append("Running it in ${StringUtils.pluralize(delay.roundedUpSeconds, "second", withNumber = true)}.") {
+                withColor(ChatFormatting.GREEN)
+            }
         }
     }
 }
--- src/main/java/at/hannibal2/skyhanni/features/commands/TransferCooldown.kt
+++ src/main/java/at/hannibal2/skyhanni/features/commands/TransferCooldown.kt
@@ -2,59 +2,29 @@ package at.hannibal2.skyhanni.features.commands
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
-import at.hannibal2.skyhanni.events.MessageSendToServerEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.DelayedRun
-import at.hannibal2.skyhanni.utils.HypixelCommands
+import at.hannibal2.skyhanni.utils.NumberUtil.roundTo
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.SkyBlockUtils
+import at.hannibal2.skyhanni.utils.inPartialSeconds
 import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
 object TransferCooldown {
 
     private val config get() = SkyHanniMod.feature.misc.commands
+
     private var lastRunCompleted: SimpleTimeMark = SimpleTimeMark.farPast()
-    private var action: (() -> Unit)? = null
 
     @HandleEvent
     fun onWorldChange() {
-        if (!config.transferCooldown || lastRunCompleted.isInFuture()) return
+        // there are several world change events per warp, but the cooldown starts on the first one
+        if (lastRunCompleted.isInFuture()) return
         lastRunCompleted = DelayedRun.runDelayed(3.seconds) {
-            if (config.transferCooldownMessage && SkyBlockUtils.inSkyBlock) {
+            if (config.transferCooldownMessage && SkyBlockUtils.inSkyBlock)
                 ChatUtils.chat("§aPlayer Transfer Cooldown has ended.")
-            }
-            action?.invoke()
-            action = null
-        }
-    }
-
-    @HandleEvent(onlyOnSkyblock = true)
-    fun onCommand(event: MessageSendToServerEvent) {
-        if (!config.transferCooldown || lastRunCompleted.isInPast()) return
-        when (event.splitMessage[0]) {
-            "/is" -> {
-                event.cancel()
-                action = { HypixelCommands.island() }
-            }
-
-            "/warp" -> {
-                event.cancel()
-                action = {
-                    HypixelCommands.warp(event.splitMessage.subList(1, event.splitMessage.size).joinToString(" "))
-                }
-            }
-
-            "/warpforge" -> {
-                event.cancel()
-                action = { HypixelCommands.warp("forge") }
-            }
-
-            "/hub" -> {
-                event.cancel()
-                action = { HypixelCommands.warp("hub") }
-            }
         }
     }
 }
